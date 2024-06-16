2dcbed6e50a810e3d24d10047eae5c8a778a221b
Mark Klepanchuk <cheapshoterr@gmail.com>
2024-06-16 15:46:30 +0300

    first commit

---
+++ web/src/components/AbnormalValues.js
@@ -0,0 +1,32 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceArea } from 'recharts';
+
+const AbnormalValues = ({ data, anomalies }) => {
+
+  const list = [...new Set(data)];
+
+  const notAnomalies = list.filter((el) => {
+    return !anomalies.includes(el);
+  })
+
+  const floor = notAnomalies[0];
+  const ceil = notAnomalies[notAnomalies.length - 1];
+
+  const chartData = list.map((el, index) => ({index, value: el}));
+
+  return (
+    <LineChart width={800} height={400} data={chartData}>
+      <XAxis type='number' dataKey="index" />
+      <YAxis type='number'/>
+      <CartesianGrid stroke="#f5f5f5" />
+      <Tooltip />
+      <Legend />
+
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+
+      <ReferenceArea y1={floor} y2={ceil} fill="green" stroke='orange' strokeWidth={1}/>
+    
+    </LineChart>
+  )
+}
+
+export default AbnormalValues;
