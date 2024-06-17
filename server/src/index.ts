937ad5f40b8fe70cf1295a3b50af484b45a7678f
Mark Klepanchuk <cheapshoterr@gmail.com>
2024-06-17 15:52:43 +0300

    heap of changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,33 @@
 import express from 'express';
+import cors from 'cors';
+import AdmZip from 'adm-zip';
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
 
 const app = express();
+app.use(cors());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/loadZipData', async (req, res) => {
+    const pathToZip = fileURLToPath(new URL('./assets/stat-data.zip', import.meta.url));
+
+    const zipBuffer = await readFile(pathToZip);
+
+    const zip = new AdmZip(zipBuffer);
+    const zipEntries = zip.getEntries();
+    const data = zipEntries.map((entry) => {
+        if (entry.entryName.includes('stat-data')) {
+            return {
+                name: entry.entryName,
+                data: entry.getData(),
+            };
+        }
+    });
+
+    const notEmptyData = data.filter((entry) => entry.data.length > 0);
+
+    res.status(200).send(notEmptyData);
 });
 
 app.listen(PORT, () => {
--- web/src/components/HomePage.js
+++ web/src/components/HomePage.js
@@ -1,8 +1,27 @@
+import React, { useState } from 'react';
+import "../styles/HomePage.scss";
 
 const HomePage = () => {
+  const [filesData, setFilesData] = useState([]);
+
+  const handleloadZipData = async () => {
+    const response = await fetch('http://localhost:3001/api/loadZipData');
+    const data = await response.json();
+
+    setFilesData(data);
+  }
+
   return (
-    <div>
-      <p>Завантажити архів данних</p>
+    <div className="home-page">
+      <button className='load-btn' onClick={handleloadZipData}>Завантажити архів данних</button>
+
+      <ul>
+        {filesData.map((file, index) => (
+          <li key={index}>
+            <p>{file.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
